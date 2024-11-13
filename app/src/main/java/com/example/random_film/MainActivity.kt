9b124e2bcdcef2034520e0a3d861046f5cf2058d
wotiwan <iwanpomogaev@yandex.ru>
2024-11-13 18:37:12 +0800

    functional added

--- app/src/main/java/com/example/random_film/MainActivity.kt
+++ app/src/main/java/com/example/random_film/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.random_film
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -18,10 +19,45 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val randomFilm: TextView = findViewById<TextView>(R.id.random_chosen_film)
+        val caution = findViewById<TextView>(R.id.caution)
+        val randomizeBtn = findViewById<Button>(R.id.randomize_btn)
+        val resetBtn = findViewById<Button>(R.id.reset_btn)
+        val films = resources.getStringArray(R.array.films)
+
+        val filmsCopy = mutableListOf<String>()
+        for (i in films) {
+            filmsCopy.add(i)
+        }
+
+        fun setRandomFilm() {
+            if (filmsCopy.size == 0) {
+                caution.visibility = View.VISIBLE
+                resetBtn.visibility = View.VISIBLE
+            } else {
+                val randomNum = (0..<filmsCopy.size).random()
+                randomFilm.text = filmsCopy[randomNum]
+                filmsCopy.removeAt(randomNum)
+            }
+        }
+
+        fun reset() {
+            for (i in films) {
+                filmsCopy.add(i)
+            }
+            caution.visibility = View.GONE
+            resetBtn.visibility = View.GONE
+            randomFilm.text = ""
+
+        }
+
+        randomizeBtn.setOnClickListener() {
+            setRandomFilm()
+        }
+
+        resetBtn.setOnClickListener() {
+            reset()
+        }
     }
 
-    val randomFilm: TextView = findViewById<TextView>(R.id.random_chosen_film)
-    val randomizeBtn: Button = findViewById<Button>(R.id.randomize_btn)
-
-    randomizeBtn
 }
