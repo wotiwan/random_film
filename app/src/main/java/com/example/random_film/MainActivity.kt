665e70145416c4aa5e8082d507e150a377d705fe
wotiwan <iwanpomogaev@yandex.ru>
2024-11-13 15:45:10 +0800

    initial commit

---
+++ app/src/main/java/com/example/random_film/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.random_film
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    val randomFilm: TextView = findViewById<TextView>(R.id.random_chosen_film)
+    val randomizeBtn: Button = findViewById<Button>(R.id.randomize_btn)
+
+    randomizeBtn
+}
